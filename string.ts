c83047cc83e9ac4740b042ad15c5dc903cb43804
weirdsim14 <sim30217@gmail.com>
2023-07-08 02:55:41 +0900

    string

--- string.ts
+++ string.ts
@@ -1,8 +1,10 @@
-let fullName: string = 'Mark Lee';
+let myName: string = "Sim";
+myName = "SSAN";
+
+let fullName: string = 'Sang Sim';
 let age: number = 39;
 
 let sentence: string = `Hello, My name is ${fullName}
 I'll be ${age + 1} years old next month.`;
 
-;
-   console.log(sentence)
+console.log(sentence)
