b65bd2d19d697acce58503cca32f1479c2a17402
weirdsim14 <sim30217@gmail.com>
2023-07-08 02:53:11 +0900

    number

--- number.ts
+++ number.ts
@@ -1,8 +1,8 @@
 let decimal: number = 6;
 
 let hex: number = 0xf00d;
-let binary: number = 0b1010;
-let octal: number = 0o744;
+let binary: number = 0b1010; // ECMAScript 2015
+let octal: number = 0o744; // ECMAScript 2015
 let notANUmber: number = NaN;
 let underscoreNum: number = 1_000_000;
 
