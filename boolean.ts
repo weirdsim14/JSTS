eacebd0bb71da1834765647fd242687193c363b6
weirdsim14 <sim30217@gmail.com>
2023-07-08 02:50:18 +0900

    boolean

--- boolean.ts
+++ boolean.ts
@@ -1,5 +1,7 @@
 let isDone: boolean = false;
 isDone = true;
+
 console.log(typeof isDone); // boolean
 
 let isOk: Boolean = true;
+let isNotOk: boolean = new Boolean(true); // when you use the new keyword, it creates a new object instead of a primitive value
